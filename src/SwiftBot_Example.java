0b3b5e444e0f969403f18b3ba4651a8821f2da91
brunoestrad <brunoestrada36@gmail.com>
2023-11-21 15:33:53 +0000

    Example

---
+++ src/SwiftBot_Example.java
@@ -0,0 +1,10 @@
+import swiftbot.SwiftBotAPI;
+
+public class SwiftBot_Example {
+	public static void main(String[] args) {
+		SwiftBotAPI API = new SwiftBotAPI();
+		
+		API.move(100, 100, 2000);
+	}
+
+}
